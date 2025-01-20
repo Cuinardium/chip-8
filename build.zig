3216a8c4c35e967103c1c239a90c0b408d07294f
Santiago Ballerini <sballerini@itba.edu.ar>
2025-01-19 21:48:10 -0300

    chore: add sdl2 library to build

--- build.zig
+++ build.zig
@@ -36,6 +36,12 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
 
+    exe.addIncludePath(b.path("./sdl2/include"));
+    exe.addLibraryPath(b.path("./sdl2/lib"));
+
+    exe.linkSystemLibrary("SDL2");
+    exe.linkLibC();
+
     // This declares intent for the executable to be installed into the
     // standard location when the user invokes the "install" step (the default
     // step when running `zig build`).
